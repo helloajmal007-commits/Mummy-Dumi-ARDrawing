10ce8d59c0914b014a5ed17ba0b58d859b693c0b
BilalSmartObject <156500397+hasnain-developer942@users.noreply.github.com>
2026-08-17 18:01:44 +0500

    Update app_open_ad_manager.dart

--- lib/app/data/services/app_open_ad_manager.dart
+++ lib/app/data/services/app_open_ad_manager.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:sketch_flow/app/data/services/ad_consent_service.dart';
